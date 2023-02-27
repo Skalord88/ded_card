f15f507512e072eefc34339032f48d0b35a5c557
Dante Mot <damiankolenda7@gmail.com>
2023-02-27 20:34:58 +0100

    up

---
+++ api/src/main/java/pl/kolendateam/dadcard/DadcardApplication.java
@@ -0,0 +1,13 @@
+package pl.kolendateam.dadcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadcardApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/pl/kolendateam/dadcard/DadcardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kolendateam.dadcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
