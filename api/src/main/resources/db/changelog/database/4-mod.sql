244696fe1f860f9dc81b123227ac418ebe64f38c
Dante Mot <damiankolenda7@gmail.com>
2023-02-28 12:12:25 +0100

    up

---
+++ api/src/main/resources/db/changelog/database/4-mod.sql
@@ -0,0 +1,81 @@
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(1, -5, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(2, -4, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(3, -4, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(4, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(5, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(6, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(7, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(8, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(9, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(12, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(13, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(14, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(15, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(16, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(17, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(18, 4, 1, 1, 1, 1, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(19, 4, 1, 1, 1, 1, 0, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(20, 5, 2, 1, 1, 1, 1, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(21, 5, 2, 1, 1, 1, 1, 0, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(22, 6, 2, 2, 1, 1, 1, 1, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(23, 6, 2, 2, 1, 1, 1, 1, 0, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(24, 7, 2, 2, 2, 1, 1, 1, 1, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(25, 7, 2, 2, 2, 1, 1, 1, 1, 0, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(26, 8, 2, 2, 2, 2, 1, 1, 1, 1, 0);
+INSERT INTO public.modabilities
+(abilities, modabilities, bonusspell1, bonusspell2, bonusspell3, bonusspell4, bonusspell5, bonusspell6, bonusspell7, bonusspell8, bonusspell9)
+VALUES(27, 8, 2, 2, 2, 2, 1, 1, 1, 1, 0);
