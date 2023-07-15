36bc642259f5e6b7b38533f7035d6453f7575ff2
Skalord88 <123017102+Skalord88@users.noreply.github.com>
2023-07-15 09:41:31 +0200

    idDTO

--- api/src/main/java/pl/kolendateam/dadcard/characterCard/dto/CreateCharacterDTO.java
+++ api/src/main/java/pl/kolendateam/dadcard/characterCard/dto/CreateCharacterDTO.java
@@ -5,10 +5,12 @@ import pl.kolendateam.dadcard.characterCard.entity.Character;
 
 @NoArgsConstructor
 public class CreateCharacterDTO {
+    public int characterId;
     public String characterName;
     public String playerName;
 
     public CreateCharacterDTO(Character character) {
+        this.characterId = character.getId();
         this.characterName = character.getCharacterName();
         this.playerName = character.getPlayerName();
     }
