688323345cf484e9ff94a16d21f35a7cb2a41684
Skalord88 <123017102+Skalord88@users.noreply.github.com>
2023-07-25 15:14:21 +0200

    weapon_dtoV1

--- api/src/main/java/pl/kolendateam/dadcard/weapons/dto/WeaponsDTO.java
+++ api/src/main/java/pl/kolendateam/dadcard/weapons/dto/WeaponsDTO.java
@@ -1,10 +1,16 @@
 package pl.kolendateam.dadcard.weapons.dto;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import pl.kolendateam.dadcard.attack.entity.SpecialAttacks;
+import pl.kolendateam.dadcard.skills.entity.ClassSkills;
 import pl.kolendateam.dadcard.weapons.entity.WeaponCategoriesEnum;
 import pl.kolendateam.dadcard.weapons.entity.WeaponNameEnum;
 import pl.kolendateam.dadcard.weapons.entity.WeaponNumericEnum;
@@ -26,7 +32,22 @@ public class WeaponsDTO {
     public String description;
 
     public WeaponsDTO(Weapons w) {
+        this.id = w.getId();
+        this.name = w.getName();
+        this.cost = w.getCost();
+        this.damage = w.getDamage();
+        this.critical = w.getCritical();
+        this.rangeIncrement = w.getRangeIncrement();
+        this.weight = w.getWeight();
+
+        Gson gson = new Gson();
+        Type listWeaponType = new TypeToken<List<WeaponCategoriesEnum>>(){}.getType();
+        ArrayList<WeaponCategoriesEnum> typ = gson.fromJson(w.getType().toString(), listWeaponType);
         
+        this.type = typ;
+        this.description = w.getDescription();
     }
+
+    
     
 }
--- api/src/main/java/pl/kolendateam/dadcard/weapons/entity/Weapons.java
+++ api/src/main/java/pl/kolendateam/dadcard/weapons/entity/Weapons.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import pl.kolendateam.dadcard.attack.entity.SpecialAttacks;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class Weapons {
 
     int id;
