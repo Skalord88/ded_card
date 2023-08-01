c1a5b0c70d9fc18b8a1977c0ea44a96fe1f51b3e
Skalord88 <123017102+Skalord88@users.noreply.github.com>
2023-08-01 19:30:17 +0200

    cos tam

--- api/src/main/java/pl/kolendateam/dadcard/weapons/entity/Weapons.java
+++ api/src/main/java/pl/kolendateam/dadcard/weapons/entity/Weapons.java
@@ -1,10 +1,5 @@
 package pl.kolendateam.dadcard.weapons.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
