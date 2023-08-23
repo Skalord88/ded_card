1acddd7a80b88351c15f8cc23a13665566ac072a
Skalord88 <123017102+Skalord88@users.noreply.github.com>
2023-08-23 12:02:30 +0200

    Update WeaponsRepository

--- api/src/main/java/pl/kolendateam/dadcard/items/weapons/repository/WeaponsRepository.java
+++ api/src/main/java/pl/kolendateam/dadcard/items/weapons/repository/WeaponsRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.kolendateam.dadcard.items.weapons.entity.Weapons;
 
-public interface WeaponsRepository extends JpaRepository<Weapons, Integer>{
+public interface WeaponsRepository extends ItemsRepository<Weapons>{}{
     
 }
