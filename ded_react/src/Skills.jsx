ed61191ae7fc960cdd996ac98613cdb2bf0083b4
DESKTOP-14SVMD1\skalo <oscar.orzepowski@gmail.com>
2023-12-07 01:23:42 +0100

    skill table

--- ded_react/src/Skills.jsx
+++ ded_react/src/Skills.jsx
@@ -33,11 +33,61 @@ export function Skills() {
         <fieldset>
             <legend>skills</legend>
         <div>
-            {skills.map((s,index) => {
-                return(
-                    <div key={index}>{s.classSkill?<>x</>:<>o</>} {s.nameSkill}</div>
-                )
-            })}
+            <table>
+                <tr>
+                    <th>CS</th>
+                    <th align='left'>skill</th>
+                    <th align='center'>add/rmv</th>
+                    <th align='center'>tot</th>
+                    <th align='center'>rnk</th>
+                    <th align='center'>abi</th>
+                    <th align='center'>dif</th>
+                </tr>
+                <tr>
+                    <td>
+                        {skills.map((s,index) => {
+                            return(
+                                <div key={index} align='center'>{s.classSkill?<>x</>:<>o</>}</div>
+                            )})}
+                    </td>
+                    <td>
+                    {skills.map((s,index) => {
+                        return(
+                            <div key={index} align='left'>{s.nameSkill}</div>
+                        )})}
+                    </td>
+                    <td>
+                    {skills.map((s,index) => {
+                        return(
+                            <div key={index} align='center'><button>+</button><button>-</button></div>
+                        )})}
+                    </td>
+                    <td>
+                    {skills.map((s,index) => {
+                        return(
+                            <div key={index} align='center'>{s.skillRank+s.skillAbility+s.skillBonus}</div>
+                        )})}
+                    </td>
+                    <td>
+                    {skills.map((s,index) => {
+                        return(
+                            <div key={index} align='center'>{s.skillRank}</div>
+                        )})}
+                    </td>
+                    <td>
+                    {skills.map((s,index) => {
+                        return(
+                            <div key={index} align='center'>{s.skillAbility}</div>
+                        )})}
+                    </td>
+                    <td>
+                    {skills.map((s,index) => {
+                        return(
+                            <div key={index} align='center'>{s.skillBonus}</div>
+                        )})}
+                    </td>
+                </tr>
+            </table>
         </div>
         </fieldset>
         :<div>...</div>
