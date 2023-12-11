9b12477ca951472a4c1cb5ca5702c776c2b989ac
Skalord88 <123017102+Skalord88@users.noreply.github.com>
2023-12-11 01:11:12 +0100

    check the list

--- ded_react/src/Skills.jsx
+++ ded_react/src/Skills.jsx
@@ -9,17 +9,18 @@ export function Skills() {
 
     const [char, setChar] = useState('');
     const [skills, setSkills] = useState('');
-    const [skillsToAdd, setSkillsToAdd] = useState('');
-    const [skillToChange, setSkillToChange] = useState('');
-    const [listToAdd, setListToAdd] = useState('')
     const [actualSkillsPoints, setActualSkillsPoints] = useState(0)
+    const [maxSkLv, setMaxSkLv] = useState(0)
+
+    const [skillName, setSkillName] = useState('');
+    const [listToAdd, setListToAdd] = useState([])
+    
 
     const [id, setId] = useState('')
     const [skCl, setSkCl] = useState('')
-    let [rank, setRank] = useState(0)
+    const [rank, setRank] = useState(0)
 
-    let [count, setCount] = useState(0);
-    let [add, setAdd] = useState([false, true]);
+    const [count, setCount] = useState(0);
 
 
     useEffect(() => {
@@ -27,8 +28,9 @@ export function Skills() {
             try {
                 const resURL = await axios.get(URL);
                 
-                setChar(resURL.data);
-                setSkills(resURL.data.skillsList);
+                setChar(resURL.data)
+                setSkills(resURL.data.skillsList)
+                setMaxSkLv(resURL.data.effectiveCharacterLv + 3)
                 setActualSkillsPoints(resURL.data.skillPoints)
                 setCount(resURL.data.skillPoints)
                 
@@ -43,81 +45,106 @@ export function Skills() {
     const handleIdSkill = (e) => {
         const buttonArray = e.target.value.split(',');
         if(Array.isArray(buttonArray) && buttonArray.length > 0){
-        setSkillsToAdd({
-            idSkill : buttonArray[0],
-            skClass : buttonArray[1],
-            skillRank : rank
-        })}
-
-        setSkillToChange(buttonArray[2])
-
-    }
-
-    console.log(skillsToAdd)
-
-    const handleRankSkill = (e) => {
-        
-        const sign = e.target.value
-
-        let updateSkill = 0;
-
-        let actualRank = skillsToAdd.skillRank
-
-        if(skillsToAdd.skClass && sign){
-            updateSkill = {...skillsToAdd, skillRank : +1}
-        } else if (skillsToAdd.skClass && !sign) {
-            updateSkill = {...skillsToAdd, skillRank : -1}
-        } else if (!skillsToAdd.skClass && sign){
-            updateSkill = {...skillsToAdd, skillRank : +0.5}
-        } else if (!skillsToAdd.skClass && !sign){
-            updateSkill = {...skillsToAdd, skillRank : -0.5}
+            setId(buttonArray[0])
+            setSkCl(buttonArray[1])
+            setSkillName(buttonArray[2])
         }
-
-        setSkillsToAdd(updateSkill)
-
-
-        // setCount(actualSkillsPoints - rank)
-
-        // if(rank > 0){
-        //     setAdd([true, true])
-        // } else if(rank < 0){
-        //     setAdd([true, false])
-        // }
-
     }
 
-    console.log(skillsToAdd)
+    const handleAddRank = () => {
+        if(-maxSkLv < (count - actualSkillsPoints)){
+            if(JSON.parse(skCl)){
+                setRank(rank + 1)
+                setCount(count - 1)
+            } else {
+                setRank(rank + 0.5)
+                setCount(count - 1)
+            }
+        }
+    }
+    const handleDelRank = () => {
+        if(actualSkillsPoints > count){
+            if(JSON.parse(skCl)){
+                setRank(rank - 1)
+                setCount(count + 1)
+            } else {
+                setRank(rank - 0.5)
+                setCount(count + 1)
+            }
+        }
+    }
+
+    console.log(id, skCl, rank, (count - actualSkillsPoints))
 
     const handleAdd = () => {
+        debugger
+        if(listToAdd.length===0){
+            setListToAdd([...listToAdd, {
+
+                idSkill : id,
+                classSkill : skCl,
+                nameSkill : skillName,
+                skillRank : rank
+                
+            }])
+        }
+        const existingSkill = listToAdd.find(e => e.idSkill === id);
         
-        setListToAdd([...listToAdd, skillsToAdd])
-        setActualSkillsPoints(prev => prev - skillsToAdd.skPoints)
-        setSkillsToAdd('')
+        if(existingSkill){
+            setListToAdd([...listToAdd, listToAdd.skillRank = rank])
+        } else {
+            setListToAdd([...listToAdd, {
+
+                idSkill : id,
+                classSkill : skCl,
+                nameSkill : skillName,
+                skillRank : rank
+                
+            }])
+        }
+        
+        setActualSkillsPoints(prev => prev + (count - actualSkillsPoints))
+        setId('')
         setRank(0)
     }
 
+    const realodPage = () => window.location.reload(false)
+
+    console.log(listToAdd)
+
     return (
         <>
-        <div>{char.characterName}, skills points: {actualSkillsPoints} / {count} rmng pts</div>
         <div>
-            {skillsToAdd?
+            {char.characterName}, skills points: {actualSkillsPoints} / {count} rmng pts <button onClick={realodPage}>reload</button>
+        </div>
+        {listToAdd.length>0?
+        <>
+        {listToAdd.map((s, index) => {
+            return (
+            <div key={index}>
+                <button
+                value={[s.idSkill, s.classSkill, s.nameSkill]}
+                onClick={handleIdSkill}
+                >
+                {s.nameSkill}</button> {s.skillRank}
+            </div>
+        )})}
+        </>
+        :
+        <></>}
+        <div>
+            {id?
             <>
-            {skillToChange}
-            {rank}
-            <button
-            value={true}
-            onClick={handleRankSkill}
-            >+</button>
-            <button
-            value={false}
-            onClick={handleRankSkill}
-            >-</button>
+            {skillName} {rank}
+            <button onClick={handleAddRank}>+</button>
+            <button onClick={handleDelRank}>-</button>
             <button onClick={handleAdd}>add</button>
             </>
-            :<></>}
+            :
+            <></>}
         </div>
-        <div>{add===true?<button>add</button>:<></>}</div>
-        <>{skills?
+        <>
+        {skills?
         <fieldset>
             <legend>skills</legend>
         <div>
@@ -148,7 +175,7 @@ export function Skills() {
                                 value={[s.idSkill, s.classSkill, s.nameSkill]}
                                 onClick={handleIdSkill}
                                 >{s.nameSkill}</button>
-                                </div>
+                            </div>
                         )})}
                     </td>
                     <td>
