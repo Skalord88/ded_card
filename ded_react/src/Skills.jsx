a81b9be3d354dfe08e7ca2b1eea5e509ae1e6aa1
DESKTOP-14SVMD1\skalo <oscar.orzepowski@gmail.com>
2023-12-12 18:15:09 +0100

    findfind

--- ded_react/src/Skills.jsx
+++ ded_react/src/Skills.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
 import axios from 'axios'
 
 export function Skills() {
@@ -7,26 +8,30 @@ export function Skills() {
     let { charId } = useParams();
     const URL = 'http://localhost:8080/character-card/'+charId;
 
-    const [char, setChar] = useState('');
-    const [skills, setSkills] = useState('');
+    //1
+    const [char, setChar] = useState('')
+    const [skills, setSkills] = useState([])
+    const [skill, setSkill] = useState({});
     const [actualSkillsPoints, setActualSkillsPoints] = useState(0)
     const [maxSkLv, setMaxSkLv] = useState(0)
 
-    const [skillName, setSkillName] = useState('');
-    const [listToAdd, setListToAdd] = useState([])
+    //6
+    const [skillName, setSkillName] = useState('')
+    const [listToAdd, setListToAdd] = useState('')
     
-
-    const [id, setId] = useState('')
+// 8
+    const [id, setId] = useState(0)
     const [skCl, setSkCl] = useState('')
     const [rank, setRank] = useState(0)
-
-    const [count, setCount] = useState(0);
-
+    const [count, setCount] = useState(0)
+//11
 
     useEffect(() => {
         const fetchData = async () => {
+            
             try {
-                const resURL = await axios.get(URL);
+                
+                const resURL = await axios.get(URL)
                 
                 setChar(resURL.data)
                 setSkills(resURL.data.skillsList)
@@ -42,25 +47,56 @@ export function Skills() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+
+    // const handleFilter = (e) => {
+
+    //     let value = e.target.value;
+
+    //     console.log('value:', value, 'skills:',skills)
+
+    //     let filtered = skills.find(sk => sk.idSkill===value);
+    //     setSkill(filtered);
+    //     console.log(filtered)
+
+    // }
+    
     const handleIdSkill = (e) => {
-        const buttonArray = e.target.value.split(',');
-        if(Array.isArray(buttonArray) && buttonArray.length > 0){
-            setId(buttonArray[0])
-            setSkCl(buttonArray[1])
-            setSkillName(buttonArray[2])
-        }
+
+        const existingSkill = skills.find(element => element.idSkill === JSON.parse(e.target.value));
+
+        setId(existingSkill.idSkill)
+        setSkillName(existingSkill.nameSkill)
+
     }
 
-    const handleAddRank = () => {
-        if(-maxSkLv < (count - actualSkillsPoints)){
-            if(JSON.parse(skCl)){
-                setRank(rank + 1)
-                setCount(count - 1)
-            } else {
-                setRank(rank + 0.5)
-                setCount(count - 1)
+    const handleAddRank = (e) => {
+
+        const updateSkills = skills.map(s => {
+
+            if(s.idSkill===id){
+                if(-maxSkLv < (count - actualSkillsPoints)){
+                    if(s.classSkill){
+                        s.skillRank++
+                        setCount(count - 1)
+                    } else {
+                        setRank(s.skillRank + 0.5)
+                        setCount(count - 1)
+                    }
+                }
             }
-        }
+        })
+
+        setSkills(updateSkills)
+
+        // if(-maxSkLv < (count - actualSkillsPoints)){
+        //     if(skill.classSkill){
+        //         setRank(skill.skillRank + 1)
+        //         setCount(count - 1)
+        //     } else {
+        //         setRank(skill.skillRank + 0.5)
+        //         setCount(count - 1)
+        //     }
+        // }
     }
     const handleDelRank = () => {
         if(actualSkillsPoints > count){
@@ -74,35 +110,14 @@ export function Skills() {
         }
     }
 
-    console.log(id, skCl, rank, (count - actualSkillsPoints))
+    // console.log(id, skCl, rank, (count - actualSkillsPoints))
 
-    const handleAdd = () => {
-        debugger
-        if(listToAdd.length===0){
-            setListToAdd([...listToAdd, {
+    const handleChange = () => {
 
-                idSkill : id,
-                classSkill : skCl,
-                nameSkill : skillName,
-                skillRank : rank
-                
-            }])
-        }
-        const existingSkill = listToAdd.find(e => e.idSkill === id);
-        
-        if(existingSkill){
-            setListToAdd([...listToAdd, listToAdd.skillRank = rank])
-        } else {
-            setListToAdd([...listToAdd, {
+        // let existingSkill = skills.find(element => element.idSkill === id);
 
-                idSkill : id,
-                classSkill : skCl,
-                nameSkill : skillName,
-                skillRank : rank
-                
-            }])
-        }
-        
+        // setSkill(existingSkill)
+
         setActualSkillsPoints(prev => prev + (count - actualSkillsPoints))
         setId('')
         setRank(0)
@@ -110,35 +125,18 @@ export function Skills() {
 
     const realodPage = () => window.location.reload(false)
 
-    console.log(listToAdd)
-
     return (
         <>
         <div>
             {char.characterName}, skills points: {actualSkillsPoints} / {count} rmng pts <button onClick={realodPage}>reload</button>
         </div>
-        {listToAdd.length>0?
-        <>
-        {listToAdd.map((s, index) => {
-            return (
-            <div key={index}>
-                <button
-                value={[s.idSkill, s.classSkill, s.nameSkill]}
-                onClick={handleIdSkill}
-                >
-                {s.nameSkill}</button> {s.skillRank}
-            </div>
-        )})}
-        </>
-        :
-        <></>}
         <div>
             {id?
             <>
             {skillName} {rank}
             <button onClick={handleAddRank}>+</button>
             <button onClick={handleDelRank}>-</button>
-            <button onClick={handleAdd}>add</button>
+            <button onClick={handleChange}>change</button>
             </>
             :
             <></>}
@@ -172,7 +170,7 @@ export function Skills() {
                         return(
                             <div key={index} align='left'>
                                 <button
-                                value={[s.idSkill, s.classSkill, s.nameSkill]}
+                                value={s.idSkill}
                                 onClick={handleIdSkill}
                                 >{s.nameSkill}</button>
                             </div>
@@ -187,12 +185,15 @@ export function Skills() {
                         )})}
                     </td>
                     <td>
-                    {skills.map((s,index) => {
-                        return(
-                            <div key={index} align='center'>
-                                {s.skillRank}
-                            </div>
-                        )})}
+                        <>
+                        {skills.map((s, index) => {
+                            return(
+                                <div key={index} align='center'>
+                                    {s.skillRank}
+                                </div>
+                            )
+                        })}
+                        </>
                     </td>
                     <td>
                     {skills.map((s,index) => {
