7f39d6cbe4704de00b153890253f4dfda1d07bbd
Skalord88 <oscar.orzepowski@gmail.com>
2024-07-15 13:24:26 +0200

    backpackIcons

--- ded_web_app/src/components/Items/Inventory/BuyBackpack.tsx
+++ ded_web_app/src/components/Items/Inventory/BuyBackpack.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { DropdownComponent } from "../../DropDown/DropDown";
 import { ListOfSomething } from "../../List/List";
 import { addToDrop } from "../../functions";
 import { Item, WonderousItem } from "../../interfaces";
+import { Popup } from "../../Popup/Popup";
+import { FilterNoItem } from "../Functions/Functions";
 
 export type Backpack = {
   item: Item[]; // elementi nello zaino
@@ -14,43 +17,73 @@ export type Backpack = {
 
 export const BuyBackpack: React.FC<Backpack> = ({
   item,
-  type,
-  text,
   items,
+  text,
+  type,
   buyItem,
   sellItem
 }) => {
-  const selectItem = (option: Item ) => {
+  const [selected, setSelected] = useState<WonderousItem[] | Item[]>(
+    FilterNoItem(item)
+  );
+  const selectItem = (option: WonderousItem | Item) => {
     buyItem(option, type);
+    setSelected((prevList) => {
+      prevList.push(option);
+      return prevList;
+    });
   };
-  const deselect = (i: WonderousItem ) => {
-    sellItem(i, type);
+  const deselect = (option: WonderousItem | Item, i: number) => {
+    const newList = selected.filter((item, index) => index !== i);
+    setSelected(newList);
+    sellItem(option, type);
   };
   const listOfItems = addToDrop(items, "normal");
 
   return (
     <>
-      <div className="rpgui-container-framed-grey" style={{ display: "grid" }}>
-        <div style={{ gridRow: 1, gridColumn: 1 }}>
-          <p>{text}</p>
-          <DropdownComponent
-            options={listOfItems}
-            onAction={selectItem}
-          />
-        </div>
+      {selected ? (
+        <>
+          <div
+            className="rpgui-container-framed-grey"
+            style={{ display: "grid" }}
+          >
+            <div style={{ gridRow: 1 }}>
+              <p>{text}</p>
+            </div>
 
-        <div style={{ gridRow: 1, gridColumn: 2 }}>
-          
-        </div>
-        <div style={{ gridRow: 1, gridColumn: "2 / span 3" }}>
-          <ListOfSomething
-            items={item}
-            text={"Backpack:"}
-            onSelect={deselect}
-          />
-        </div>
-        <div style={{ gridRow: 2 }} className="rpgui-icon backpack"></div>
-      </div>
+            <div
+              className="rpgui-list-imp"
+              style={{
+                height: 120,
+                fontSize: "60%",
+                gridRow: 2,
+                gridColumn: 2
+              }}
+            >
+              {selected.map((item, index) => {
+                return (
+                  <>
+                    <div onClick={() => deselect(item, index)}>
+                      <Popup text={item.name} popText={item.description} />
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+            <div style={{ gridRow: 2, gridColumn: 3 }}>
+              <DropdownComponent
+                options={listOfItems}
+                onAction={selectItem}
+                key={"drop " + type}
+              />
+            </div>
+            <div style={{ gridRow: 2 }} className="rpgui-icon backpack"></div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
