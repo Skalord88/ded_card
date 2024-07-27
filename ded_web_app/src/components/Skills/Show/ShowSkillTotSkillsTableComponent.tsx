287e622230527a5b730fad0e95bad17ba56500f2
Skalord88 <oscar.orzepowski@gmail.com>
2024-07-27 15:54:04 +0200

    move

--- ded_web_app/src/components/Skills/Show/ShowSkillTotSkillsTableComponent.tsx
+++ ded_web_app/src/components/Skills/Show/ShowSkillTotSkillsTableComponent.tsx
@@ -1,15 +1,14 @@
 import { ShowOneSkillProps } from "../../interfaces";
 
 export const ShowSkillTotSkillsTableComponent: React.FC<ShowOneSkillProps> = ({
-    skill
-  }) => {
-  
-    return (
-      <div
-        className="rpgui-container-framed-grey-mini"
-        style={{ color: "orange" }}
-      >
-        {skill.skillRank + skill.skillAbility + skill.skillBonus}
-      </div>
-    );
-  };
+  skill
+}) => {
+  return (
+    <div
+      className="rpgui-container-framed-grey-mini"
+      style={{ color: "orange" }}
+    >
+      {skill.skillRank + skill.skillAbility + skill.skillBonus}
+    </div>
+  );
+};
