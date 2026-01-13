9321f6748a3b38089f1e7c8764cba84a87a6a891
Skalord88 <oscar.orzepowski@gmail.com>
2026-01-13 12:31:44 +0100

    aligmDei

--- api/src/main/java/pl/kolendateam/dadcard/race/MapperRaceToDTO.java
+++ api/src/main/java/pl/kolendateam/dadcard/race/MapperRaceToDTO.java
@@ -1,5 +1,6 @@
 package pl.kolendateam.dadcard.race;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -82,6 +83,7 @@ public class MapperRaceToDTO {
         }
       });
     }
+
     return deitisDTOSet;
   }
 
